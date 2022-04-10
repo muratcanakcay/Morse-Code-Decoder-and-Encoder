998ea107df5abe0485d9e58d6c9c0b3d9352625b
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-10 03:19:00 +0200

    Bug: receiving 2 events when it should receive 1

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
+#define _GNU_SOURCE
+
 #include <gpiod.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "time.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
 
 #ifndef	CONSUMER
 #define	CONSUMER	"Consumer"
 #endif
+
+#define READ 1
+
+typedef unsigned int UINT;
+typedef struct timespec timespec_t;
+
+void msleep(UINT milisec) 
+{
+    time_t sec= (int)(milisec/1000);
+    milisec = milisec - (sec*1000);
+    timespec_t req= {0};
+    req.tv_sec = sec;
+    req.tv_nsec = milisec * 1000000L;
+    if(nanosleep(&req,&req)) perror("nanosleep");
+}
 
 int main(int argc, char **argv)
 {
@@ -14,7 +40,7 @@ int main(int argc, char **argv)
 	struct gpiod_line_event event;
 	struct gpiod_chip *chip;
 	struct gpiod_line *line;
-	int i, ret;
+	int i, ret, newVal, oldVal;
 
 	chip = gpiod_chip_open_by_name(chipname);
 	if (!chip) {
@@ -28,41 +54,102 @@ int main(int argc, char **argv)
 		perror("Get line failed\n");
 		ret = -1;
 		goto close_chip;
-	}
-
-	ret = gpiod_line_request_both_edges_events(line, CONSUMER);
-	if (ret < 0) {
-		perror("Request event notification failed\n");
-		ret = -1;
-		goto release_line;
-	}
+	}	
 
 	/* Notify event up to 20 times */
 	//i = 0;
-	//while (i <= 20) 
-	while (true) {
-		ret = gpiod_line_event_wait(line, &ts);
-		if (ret < 0) {
-			perror("Wait event notification failed\n");
-			ret = -1;
-			goto release_line;
-		} else if (ret == 0) {
+	//while (i <= 20)
+	int lastEventTime = 0; 
+	while (true) 
+    {
+		
+        // request events
+        ret = gpiod_line_request_both_edges_events(line, CONSUMER);
+	    if (ret < 0) 
+        {
+		    perror("Request event notification failed\n");
+		    ret = -1;
+		    goto release_line;
+        }
+
+        // wait for event
+        ret = gpiod_line_event_wait(line, &ts);
+		if (ret < 0) 
+        {
+            perror("Wait event notification failed\n");
+            ret = -1;
+            goto release_line;
+        } 
+        else if (ret == 0) 
+        {
 			printf("Wait event notification on line #%u timeout\n", line_num);
-			continue;
-		}
-
-		ret = gpiod_line_event_read(line, &event);
-		printf("Get event notification on line #%u %d times\n", line_num, i);
-		printf("Event type: %d\n", event.event_type);
-		printf("Event time: %d sec %lo nsec\n", event.ts.tv_sec, event.ts.tv_nsec);
-
-		if (ret < 0) {
-			perror("Read last event notification failed\n");
-			ret = -1;
 			goto release_line;
 		}
-		//sleep(1);
 
+        printf("Event received!\n");
+
+        if(READ)
+        {
+            // release line from event
+            gpiod_line_release(line);
+
+            //request input        
+            ret = gpiod_line_request_input(line, CONSUMER);
+            if (ret < 0) 
+            {
+                perror("Request line as input failed\n");
+                goto release_line;
+            }
+
+            int count = 0;
+            oldVal = -1;
+            while (true)
+            {
+                newVal = gpiod_line_get_value(line);
+                
+                if (newVal < 0) 
+                {
+                    perror("Read line input failed\n");
+                    goto release_line;
+                }
+
+                printf("Input %d on line #%u\n", newVal, line_num);
+
+                if (newVal == oldVal)
+                {
+                    count++;
+                    if (count>=3) break;
+                    msleep(50);
+                    continue;
+                }
+
+                count = 0;
+                oldVal = newVal;
+                msleep(50);
+            }
+
+            printf("FINAL Input %d on line #%u\n", newVal, line_num);
+
+            gpiod_line_release(line);
+        }
+        else
+        {
+            ret = gpiod_line_event_read(line, &event);
+
+            printf("Get event notification on line #%u %d times\n", line_num, i);
+            printf("Event type: %d\n", event.event_type);
+            printf("Event time: %lo sec %lo nsec\n", event.ts.tv_sec, event.ts.tv_nsec);
+
+            if (ret < 0) {
+                perror("Read last event notification failed\n");
+                ret = -1;
+                goto release_line;
+            }
+
+            gpiod_line_release(line);
+        }
+
+		//sleep(1);
 		//i++;
 	}
 
