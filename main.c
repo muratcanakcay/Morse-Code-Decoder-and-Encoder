bc22479fed068650bf909dee006c9be008f27c96
Muratcan Akcay <muratcanakcay@gmail.com>
2022-04-11 00:23:12 +0200

    code cleanup

--- main.c
+++ main.c
@@ -3,15 +3,7 @@
 #include <gpiod.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "time.h"
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <limits.h>
 
 #ifndef	CONSUMER
 #define	CONSUMER	"Consumer"
@@ -19,8 +11,8 @@
 
 #define READ 1
 #define APP_TIMEOUT 10000               // depress duration [ms] for input end
-#define SYMBOLS_TIMEOUT 1500              // depress duration [ms] for space char
-#define SPACE_TIMEOUT 3000              // depress duration [ms] for space char
+#define SYMBOLS_TIMEOUT 1500            // depress duration [ms] for signalling end of pattern
+#define SPACE_TIMEOUT 4500              // depress duration [ms] for space char
 #define SHORT_PRESS_TIMEOUT 1500        // max. press duration [ms] for dot char
 #define BOUNCE_TIMEOUT 100              // max. bounce interval [ms]
 
@@ -86,13 +78,23 @@ void msleep(UINT milisec)
 int main(int argc, char **argv)
 {
 	char *chipname = "gpiochip0";
-	unsigned int line_num = 13;	// GPIO Pin #13
-	struct timespec bounceTimeout = { 0, BOUNCE_TIMEOUT * 1000000 };
-    struct timespec appTimeout = { APP_TIMEOUT/1000, 0 };
+	UINT line_num = 13;	// GPIO Pin #13
 	struct gpiod_line_event event;
 	struct gpiod_chip *chip;
 	struct gpiod_line *line;
-	int i, l, ret, newVal, oldVal;
+
+    timespec_t bounceTimeout = { 0, BOUNCE_TIMEOUT * 1000000 };
+    timespec_t appTimeout = { APP_TIMEOUT/1000, 0 };
+    timespec_t prevTime, lastTime;
+	
+    int ret, newVal, oldVal;
+    int morse_code_array_size = sizeof(MORSE_CODE) /  sizeof(char[6]);
+    int i = -1; // symbols index
+    int l = 0;  // letters index    
+    int prevVal = 1; // starting value of input
+
+    char symbols[500] = "\0"; // will be [6] at the end
+    char letters[500] = "\0";
 
 	chip = gpiod_chip_open_by_name(chipname);
 	if (!chip) {
@@ -106,22 +108,9 @@ int main(int argc, char **argv)
 		perror("Get line failed\n");
 		ret = -1;
 		goto close_chip;
-	}	
-
-	
-	i = -1; // symbols index
-    l = 0;  // letters index
-    bool secondEvent = false;
-    bool firstLetter = true;
-    int prevVal = 1;
-    timespec_t prevTime;
-    timespec_t lastTime;
-
-    char symbols[500] = "\0"; // will be [6] at the end
-    char letters[500] = "\0";
-
-    printf("Size of MORSE_CODE array = %lu\n", sizeof(MORSE_CODE)/sizeof(char[6]));
+	}
     
+    // main loop
     while (true)
     {
         // request events
@@ -133,8 +122,7 @@ int main(int argc, char **argv)
 		    goto release_line;
         }        
 
-        ret = gpiod_line_event_wait(line, &appTimeout);
-        if (ret < 0) 
+        if ((ret = gpiod_line_event_wait(line, &appTimeout)) < 0)        
         {
             perror("Wait event notification failed\n");
             ret = -1;
@@ -146,37 +134,36 @@ int main(int argc, char **argv)
             goto release_line;
         }
 
-        ret = gpiod_line_event_read(line, &event);
-        if (ret < 0) 
+        if ((ret = gpiod_line_event_read(line, &event)) < 0)        
         {
             perror("Read last event notification failed\n");
             ret = -1;
             goto release_line;
         }
 
-        lastTime = event.ts;
+        /* informative code */
+        // lastTime = event.ts;         
         // printf("\n");
         // printf("INITIAL event notification on line #%u %d times\n", line_num, i);
         // printf("Event type: %d\n", event.event_type);
         // printf("Event time: %lo sec %lo nsec\n", lastTime.tv_sec, lastTime.tv_nsec);
         // printf("\n");
         
-        while(true) // wait for bounce events to end
+        // debouncing loop - wait for bounce events to end
+        while(true)
         {
-            ret = gpiod_line_event_wait(line, &bounceTimeout);
-            if (ret < 0) 
+            if ((ret = gpiod_line_event_wait(line, &bounceTimeout)) < 0) 
             {
                 perror("Wait event notification failed\n");
                 ret = -1;
                 goto release_line;
             }
-            if (ret == 0) // timeout
+            else if (ret == 0) // bouncing ended - exit loop
             {
                 break;
             }
 
-            ret = gpiod_line_event_read(line, &event);
-            if (ret < 0) 
+            if ((ret = gpiod_line_event_read(line, &event)) < 0) 
             {
                 perror("Read last event notification failed\n");
                 ret = -1;
@@ -184,6 +171,8 @@ int main(int argc, char **argv)
             }
 
             lastTime = event.ts;
+            
+            /* informative code */
             //printf("\n");
             //printf("BOUNCE event notification on line #%u %d times\n", line_num, i);
             //printf("Event type: %d\n", event.event_type);
@@ -195,32 +184,30 @@ int main(int argc, char **argv)
         // printf("End of bounces!\n");
         // printf("\n");
         
-        // release line from event
+        // release line from event request
         gpiod_line_release(line);
 
-        //request input        
-        ret = gpiod_line_request_input(line, CONSUMER);
-        if (ret < 0) 
+        // request input        
+        if ((ret = gpiod_line_request_input(line, CONSUMER)) < 0) 
         {
             perror("Request line as input failed\n");
             goto release_line;
         }
 
-        newVal = gpiod_line_get_value(line);
-        if (newVal < 0) 
+        if ((newVal = gpiod_line_get_value(line)) < 0) 
         {
             perror("Read line input failed\n");
             goto release_line;
         }
 
+        /* informative code */
         //printf("Input %d on line #%u\n", newVal, line_num);
-
         // printf("PREV Input %d on line #%u\n", prevVal, line_num);
         // printf("FINAL Input %d on line #%u\n", newVal, line_num);
-
         // printf("lastTime: %ld.%ld\n", lastTime.tv_sec, lastTime.tv_nsec);
         // printf("prevTime: %ld.%ld\n", prevTime.tv_sec, prevTime.tv_nsec);
 
+        // calculate duration between previous input and current input
         int duration = (int)((lastTime.tv_sec - prevTime.tv_sec) * 1000 + (lastTime.tv_nsec - prevTime.tv_nsec) / 1000000); 
         printf("%d -> %dms -> %d\n", prevVal, duration, newVal);        
 
@@ -269,11 +256,13 @@ int main(int argc, char **argv)
                 }
 
                 printf("The pattern '%s' is at index %d and matches to %c\n", symbols, idx, idx+32);
+                
+                // clear symbols[] and insert corresponding letter in letters[]
                 i = -1;
                 symbols[0] = '\0';
-
                 letters[l] = ' ' + idx;
-                if (idx == sizeof(MORSE_CODE) /  sizeof(char[6]))
+                
+                if (idx == morse_code_array_size)
                 {
                     letters[l] = '#';   // error in pattern
                 }
@@ -294,7 +283,7 @@ int main(int argc, char **argv)
                 printf("[%d] Not adding anything\n", i);
             }
             
-            printf("\n");            
+            printf("\n");
         }
         else
         {
@@ -306,13 +295,10 @@ int main(int argc, char **argv)
         printf("Symbols: '%s' (%lu)\n", symbols, strlen(symbols));
         printf("Letters: '%s' (%lu)\n", letters, strlen(letters));
         
-        if (strlen(symbols) > 5)
+        if (strlen(symbols) > 5) // TODO: should reset symbols[]
         {
             printf("INPUT TOO LONG!\n\n");
         }
-    
-
-
 
         gpiod_line_release(line);
 	} // end main loop
